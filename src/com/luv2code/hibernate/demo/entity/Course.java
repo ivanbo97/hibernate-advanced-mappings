59244c2b35d448337d9512cfd40932d53bd4684d
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-11 06:50:42 +0200

    minor code reformatting

--- src/com/luv2code/hibernate/demo/entity/Course.java
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -27,46 +27,37 @@ public class Course {
 	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
 	
-	
 	public Course() {
 		
 	}
-
 
 	public Course(String titleString) {
 		this.title = titleString;
 	}
 
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getTitleString() {
 		return title;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
 	public Instructor getInstructor() {
 		return instructor;
 	}
 
-
 	public void setInstructor(Instructor instructor) {
 		this.instructor = instructor;
 	}
-
 
 	@Override
 	public String toString() {
