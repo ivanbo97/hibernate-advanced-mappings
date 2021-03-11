9275dc80954503cd51827ea195a43249aeb89dfa
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-11 17:14:43 +0200

    Class for demonstrating the eager and lazy loading.

---
+++ src/com/luv2code/hibernate/demo/EagerLazyLoadingDemo.java
@@ -0,0 +1,53 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+
+
+
+public class EagerLazyLoadingDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+				SessionFactory factory = new Configuration()
+										 .configure("hibernate.cfg.xml")
+										 .addAnnotatedClass(Instructor.class)
+										 .addAnnotatedClass(InstructorDetail.class)
+										 .addAnnotatedClass(Course.class)
+										 .buildSessionFactory();
+				
+				Session session = factory.getCurrentSession();
+				
+				try {
+					
+					
+					// start transaction
+					session.beginTransaction();
+					
+					// get instructor from db
+					int instructorId= 1;
+					Instructor tempInstructor = session.get(Instructor.class, instructorId);
+					
+					// retrieve the courses for this instructor
+					System.out.println("luv2code: Instructor: " + tempInstructor);
+					System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+					// commit transaction			
+					session.getTransaction().commit();
+					System.out.println("luv2code: Done!");
+					
+				} finally {
+					factory.close();
+					session.close();
+				}
+				
+
+	}
+
+}
