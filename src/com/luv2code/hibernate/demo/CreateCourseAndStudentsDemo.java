fd6b2ecf4192a99089f378c596d4c6e229dc44b8
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-12 15:20:51 +0200

    Testing the method which adds students to courses.

---
+++ src/com/luv2code/hibernate/demo/CreateCourseAndStudentsDemo.java
@@ -0,0 +1,73 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.Student;
+
+
+
+
+public class CreateCourseAndStudentsDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+				SessionFactory factory = new Configuration()
+										 .configure("hibernate.cfg.xml")
+										 .addAnnotatedClass(Instructor.class)
+										 .addAnnotatedClass(InstructorDetail.class)
+										 .addAnnotatedClass(Course.class)
+										 .addAnnotatedClass(Review.class)
+										 .addAnnotatedClass(Student.class)
+										 .buildSessionFactory();
+				
+				Session session = factory.getCurrentSession();
+				
+				try {
+					
+					
+					// start transaction
+					session.beginTransaction();
+					
+					// create a course
+					Course tempCourse = new Course("Pacman - How to score one million points?");
+					
+					// save the course
+					System.out.println("\nSaving the course...");
+					session.save(tempCourse);
+					System.out.println("Saved course: " + tempCourse);
+					
+					// create students
+					Student student1 = new Student("John","Doe","doe@luv2code.com");
+					Student student2 = new Student("Mar","Public","mary@luv2code.com");
+					
+					
+					// add students to the course
+					tempCourse.addStudent(student1);
+					tempCourse.addStudent(student2);
+					
+					// save students
+					System.out.println("Saving students...");
+					session.save(student1);
+					session.save(student2);
+					System.out.println("Saved students: " + tempCourse.getStudents());
+					
+					// commit transaction			
+					session.getTransaction().commit();
+					System.out.println("Done!");
+					
+				} finally {
+					factory.close();
+					session.close();
+				}
+				
+
+	}
+
+}
